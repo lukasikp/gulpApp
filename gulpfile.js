1be8ba4f4cfb937dbbe8aa8cce3a39762ecb3cad
lukasikp <lukasik.paulina@gmail.com>
2015-11-15 18:56:01 +0100

    dodanie mapowania styli

--- gulpfile.js
+++ gulpfile.js
@@ -1,11 +1,14 @@
 var gulp = require('gulp');
 var less = require('gulp-less');
-var path = require('path');
+var sourcemaps = require('gulp-sourcemaps');
 
 gulp.task('default',function (){
 	gulp.src('public/style/less/**/*.less')
+	.pipe(sourcemaps.init())
 	.pipe(less())
+	.pipe(sourcemaps.write('.'))
 	.pipe(gulp.dest('public/style'));
+
 	// ** - wszystkie pliki z folderu
 	//*.less - konczonce sie na less
 });
